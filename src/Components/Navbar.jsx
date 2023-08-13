4f04449a5a5bbbf0cc5515cb54606361929e4964
Coderacademics <saikatmohanta79@gmail.com>
2023-08-13 22:14:39 +0530

    Made the Navbar responsive

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../CSS Styling/Navbar.css';
 import { HashLink as Link } from 'react-router-hash-link';
-import Logo from '../pictures/logo.png'
+import Logo from '../pictures/logo.png';
+import MenuIcon from '@mui/icons-material/Menu';
 function Navbar() {
     const [navbarClass, setNavbarClass] = useState('navbar-container');
+    const [mobileNavVisible, setMobileNavVisible] = useState(false)
     window.addEventListener("scroll", () => {
         if (window.scrollY === 0)
             setNavbarClass('navbar-container');
         else
             setNavbarClass('navbar-container scrolled');
     })
+
     return <>
         <div className={navbarClass}>
             <img src={Logo} height={50} width={120} />
-            <div className="nav-items-container">
+            <div className={mobileNavVisible? "nav-items-container visible" :"nav-items-container invisible"}>
                 <li><Link smooth to="#" className="nav-items">HOME</Link></li>
                 <li ><Link smooth to="#aboutme" className="nav-items">ABOUT</Link></li>
                 <li><Link smooth to="#skills" className="nav-items">SKILLS</Link></li>
@@ -22,6 +25,9 @@ function Navbar() {
                 <li ><Link smooth to="#contactme" className="nav-items">CONTACT</Link></li>
             </div>
             <Link smooth to="#contactme" style={{ textDecoration: "none", color: "#fff" }}><button className="hire-me-btn">HIRE ME</button></Link>
+            <div className="hamburger-menu" onClick={()=>{
+                setMobileNavVisible(!mobileNavVisible)
+            }}><MenuIcon/></div>
         </div>
     </>;
 };
